f7144c82da565784efa178a861f0afeb2744187c
Gabriel Yagoub <gabrielyagoub@gmail.com>
2023-07-03 17:50:32 +0200

    "Products" done, now working on "Orders"

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,9 +1,20 @@
 var express = require('express');
+const { ObjectId } = require('mongodb');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Du är nu inne i orders routern!');
+// SKAPA ORDER FÖR EN SPECIFIK USER // PRODUCTS ÄR EN ARRAY MOTSVARANDE INNEHÅLLET I KUNDVAGN
+router.post('/add', function(req, res, next) {
+  const completeOrder = {products: req.body.products, user: req.body.user}; 
+  req.app.locals.db.collection("orders").insertOne(completeOrder)
+  .then(result => {
+    console.log(completeOrder);
+    res.json(result);
+  })
 });
 
+
+
 module.exports = router;
+
+
+
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,9 +1,30 @@
 var express = require('express');
+const { ObjectId } = require('mongodb');
 var router = express.Router();
 
-/* GET users listing. */
+// HÄMTA ALLA PRODUKTER
 router.get('/', function(req, res, next) {
-  res.send('Du är nu inne i products routern!');
+  req.app.locals.db.collection('products').find().toArray()
+  .then(result => {
+    res.json(result)
+  })
+  .catch(error => console.error(error, "Ett fel uppstod när alla produkter skulle hämtas"))
+});
+
+
+// HÄMTA SPECIFIK PRODUKT
+router.get('/:id', function(req, res, next) {
+  req.app.locals.db.collection('products').findOne({"_id" : new ObjectId(req.params.id)})
+  .then(result => {
+    res.json(result)
+  });
+});
+
+// SKAPA PRODUKT 
+router.post('/add', function(req, res, next) {
+  const newProduct = req.body;
+  req.app.locals.db.collection('products').insertOne(newProduct)
+  res.send("Produkt tillagd");
 });
 
 module.exports = router;
