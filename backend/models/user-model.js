3f29638f557137bc00bb63fd484da05ec2f3cf2d
Gabriel Yagoub <gabrielyagoub@gmail.com>
2023-07-05 15:52:11 +0200

    Last commit

--- backend/models/user-model.js
+++ backend/models/user-model.js
@@ -7,3 +7,6 @@ const userSchema = mongoose.Schema({
 });
 
 module.exports = mongoose.model('user', userSchema);
+
+// Det går att lägga in "unique" på t.ex email för att inte kunna skapa flera användare med samma email.
+// I syfte att kunna testa koden med så lite besvär som möjligt så har jag valt att inte göra det.
--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -3,56 +3,31 @@ var router = express.Router();
 const OrderModel = require('../models/order-model');
 const ProductModel = require('../models/product-model');
 
-// Skapa ny order för en specifik användare
+// Skapa ny order för en specifik användare - OK!
 router.post('/add', async (req, res) => {
     const {products} = req.body;
     for (const productItem of products) {
-        const product = await ProductModel.findById(productItem.productId);
-        product.lager = product.lager - productItem.quantity;
-        await product.save();
-    } 
-    const newOrder = await OrderModel.create(req.body);
-    res.json(newOrder);
+        try {
+            const product = await ProductModel.findById(productItem.productId);
+            product.lager = product.lager - productItem.quantity;
+            await product.save();
+        } catch(error) {
+            res.status(404).json("Felaktigt produkt-id");
+            return;
+        }
+    }
+    try{
+        const newOrder = await OrderModel.create(req.body);
+        res.json(newOrder);
+    } catch(error) {
+        res.status(404).json("Felaktigt användar-id");
+    }
 });
 
-
-
-
-
-
-
-
-// // Skapa ny order för en specifik användare
-// router.post('/add', async (req, res) => {
-//     const {products} = req.body;
-//     for (const productItem of products) {
-//         const product = await ProductModel.findById({_id: productItem.productId});
-//         product.lager = product.lager - productItem.quantity;
-//         await product.save();
-//     }
-// });
-
-// // Skapa ny order för en specifik användare
-// router.post('/add', async (req, res) => {
-//     try {
-//         let products = req.body;
-//         const newOrder = await OrderModel.create(req.body);
-//         let product = await ProductModel.findById({_id: products.productId});
-//         product.lager = product.lager - req.body.quantity;
-//         console.log();
-//         res.json(newOrder);
-//     } catch(error){
-//         res.status(404).json("Felaktigt användar id eller produkt id");
-//     }
-// });
-
-
-
-
-// Hämta alla ordrar
+// Hämta alla ordrar - OK!
 router.get('/all', async (req, res) => {
     const allOrders = await OrderModel.find();
-    res.json(allOrders);
+    res.status(200).json(allOrders);
 })
 
 
