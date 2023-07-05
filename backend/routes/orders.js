27f7328ebf2a2a4ba56d842297d05a6b10b0b289
Gabriel Yagoub <gabrielyagoub@gmail.com>
2023-07-05 11:15:10 +0200

    Commit

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -2,19 +2,38 @@ var express = require('express');
 const { ObjectId } = require('mongodb');
 var router = express.Router();
 
-// SKAPA ORDER FÖR EN SPECIFIK USER // PRODUCTS ÄR EN ARRAY MOTSVARANDE INNEHÅLLET I KUNDVAGN
-router.post('/add', function(req, res, next) {
-  const completeOrder = {products: req.body.products, user: req.body.user}; 
-  req.app.locals.db.collection("orders").insertOne(completeOrder)
-  .then(result => {
-    console.log(completeOrder);
-    res.json(result);
-  })
+// router.post('/add', async function(req, res, next) {
+//   const productItems = req.app.locals.db.collection("products").findOne({"_id": new ObjectId(req.body.productId)});
+
+//   const user = await req.app.locals.db.collection("users").findOne({"_id": new ObjectId(req.body.user)});
+//   const newOrder = {user: user._id, products: req.body.products}
+//   const result = await req.app.locals.db.collection("orders").insertOne(newOrder);
+//   res.json(result); 
+
+// });
+
+
+router.post('/add', async (req, res) => {
+  // Extract user and products data from the request body
+  const { user, products } = req.body;
+  req.app.locals.db.collection("users").findOne({"_id": new ObjectId(req.body.user)});
+  //req.app.locals.db.collection("products").findOne({"_id": {$in: products}});
+
+  const productIds = products.map(product => product);
+  const matchingProducts = await req.app.locals.db.collection('products').findOne({ _id: { $in: productIds } });
+
+  // if (matchingProducts !== productIds) {
+  //   return res.status(404).json({ error: 'One or more products not found' });
+  // }
+
+  const order = {
+    user,
+    products
+  };
+
+  await req.app.locals.db.collection("orders").insertOne(order);
+
+  res.json({ message: 'Order created successfully', order });
 });
 
-
-
 module.exports = router;
-
-
-
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -13,24 +13,6 @@ router.get('/', function(req, res, next) {
 });
 
 
-// // HÄMTA SPECIFIK USER // SKICKA HELA OBJEKTET
-// // router.post('/', function(req, res, next) {
-// //   const specificId = req.body.id;
-// //   const user = users.find(user => user.id == specificId);
-// //   res.json(user);
-// // });
-
-// router.post('/', function(req, res, next) {
-//   const specificId = req.body.id;
-//   const foundUserId = users.find(user => user.id == specificId);
-//   if (foundUserId) {
-//     res.json(foundUserId);
-//   }
-//   else {
-//     res.send("Ingen användare med detta id hittades");
-//   };
-// });
-
 // HÄMTA SPECIFIK USER // SKICKA HELA OBJEKTET
 router.post('/', function(req, res, next) {
   req.app.locals.db.collection('users').findOne({"_id" : new ObjectId(req.body.id)})
